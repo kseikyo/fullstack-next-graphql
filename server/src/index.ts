3b3d0279b22045050d299a857327af643e76239b
Lucas Emanuel <lucasemanuelss@hotmail.com>
2020-12-13 14:55:57 -0300

    Initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,65 @@
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./contants";
+import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
+
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+// import { MyContext } from "./types";
+
+const main = async () => {
+  // Connect to database
+  const orm = await MikroORM.init(microConfig);
+  // Run migrations
+  await orm.getMigrator().up();
+
+  const app = express();
+
+  // Redis connection
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  // middleware to use redis to store the session cookie
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years of duration
+        httpOnly: true,
+        sameSite: "lax",
+        secure: __prod__ // cookie only works in https
+      },
+      saveUninitialized: false,
+      secret: "supersecret",
+      resave: false,
+    })
+  );
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver, UserResolver],
+      validate: () => {},
+    }),
+    context: ({ req, res }) => ({ em: orm.em, req, res }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(process.env.PORT || 3333, () => {
+    console.log("server started");
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
+});
---
+++ server/src/resolvers/user.ts
@@ -0,0 +1,145 @@
+import { User } from "../entities/User";
+import { MyContext } from "../types";
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+} from "type-graphql";
+import argon2 from "argon2";
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string;
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
+@Resolver()
+export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req, em }: MyContext) {
+    const id = req.session.userId;
+    // User not logged in
+    if (!id) {
+      return null;
+    }
+    const user = await em.findOne(User, { id });
+    return user;
+  }
+
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em, req }: MyContext
+  ): Promise<UserResponse> {
+    if (options.username.length === 0) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "Username cannot be empty",
+          },
+        ],
+      };
+    }
+
+    if (options.password.length === 0) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Password cannot be empty",
+          },
+        ],
+      };
+    }
+
+    const hashedPassword = await argon2.hash(options.password);
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedPassword,
+    });
+
+    try {
+      await em.persistAndFlush(user);
+    } catch (err) {
+      if (err.code === "23505") {
+        return {
+          errors: [
+            {
+              field: "username",
+              message: "That username is taken",
+            },
+          ],
+        };
+      }
+    }
+
+    // store user id session
+    // TODO remove session and use JWT
+    req.session.userId = user.id;
+
+    return { user };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em, req }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username });
+
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "Username incorrect",
+          },
+        ],
+      };
+    }
+
+    const valid = await argon2.verify(user.password, options.password);
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Password incorrect",
+          },
+        ],
+      };
+    }
+
+    // Store the userId on session
+    req.session.userId = user.id;
+
+    return {
+      user,
+    };
+  }
+}
